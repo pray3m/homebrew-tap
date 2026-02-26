12017459d1f92dd10277f4d42f0c8f4e66627ab6
pray3m <prem.gtm9@gmail.com>
2026-02-26 12:32:09 +0545

    fix: update app path to support wildcard for aaza.app

--- casks/aaza.rb
+++ casks/aaza.rb
@@ -1,6 +1,6 @@
 cask "aaza" do
   version "1.0.1"
-  sha256 "d620daca050f4248e6461017fbf3e5df2353078c6b09964aaa175378328e97e4"
+  sha256 "e829af6c9297c9c9ffcbd68d4af4df6c3c33d55737d3fae5579b4e96e80e3147"
 
   url "https://github.com/pray3m/aaza/releases/download/v#{version}/aaza-v#{version}-macOS.zip"
   name "aaza"
